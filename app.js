437aa785aa23bcfc7216ae8c6234f873ffe0ffe5
Soumendu Nayak <62599267+somuOO7@users.noreply.github.com>
2020-06-11 10:46:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-var typeHere = document.getElementById('typeHere');
-var characters = document.getElementById('characters');
+var typeHere = document.getElementById('typeHere'); //typing area
+var characters = document.getElementById('characters'); //counting characters below type area
 var charactersRemaining = document.getElementById('charactersRemaining');
-var words = document.getElementById('words');
+var words = document.getElementById('words'); //counting words below type area
 var wordsRemaining = document.getElementById('wordsRemaining');
+var displayCharacterLimit = document.getElementById('character-limit'); //character limit above type area
+var displayWordLimit = document.getElementById('word-limit'); //word limit above type area
+
+do {
+	var characterLimit = prompt(
+		'Please enter the character limit (numbers only)'
+	);
+} while (isNaN(characterLimit));
+
+do {
+	var wordLimit = prompt('Please enter the word limit (numbers only)');
+} while (isNaN(wordLimit));
+
+displayWordLimit.innerHTML = wordLimit;
+displayCharacterLimit.innerHTML = characterLimit;
 
 typeHere.addEventListener('keyup', (e) => {
 	characters.innerHTML = typeHere.value.length;
-	charactersRemaining.innerHTML = 100 - typeHere.value.length;
+	charactersRemaining.innerHTML = characterLimit - typeHere.value.length;
 
-	if (typeHere.value.length > 99) {
-		typeHere.value = typeHere.value.substring(0, 99);
+	if (typeHere.value.length > characterLimit - 1) {
+		typeHere.value = typeHere.value.substring(0, characterLimit - 1);
 	}
 
 	function wordCount(str) {
@@ -17,9 +32,13 @@ typeHere.addEventListener('keyup', (e) => {
 		str = str.replace(/[ ]{2,}/gi, ' ');
 		str = str.replace(/\n /, '\n');
 
-		words.innerHTML = str.split(' ').length;
+		if (typeHere.value === '') {
+			words.innerHTML = 0;
+		} else {
+			words.innerHTML = str.split(' ').length;
+		}
 	}
 
 	wordCount(typeHere.value);
-	wordsRemaining.innerHTML = 50 - words.innerHTML;
+	wordsRemaining.innerHTML = wordLimit - words.innerHTML;
 });
